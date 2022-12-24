ea3fdc50b5beb16061ed26c43c5f4e52778b428e
Dan Stroot <dan@thestroots.com>
2022-12-24 10:34:43 -0800

    cleanup

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>DreamAweso &#8226; me</title>
+      <title>DreamAweso&#8226;me</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Use AI to analyze your dreams" />
       <link rel="icon" href="/favicon.png" />
